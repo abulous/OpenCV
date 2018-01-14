a384df32dba93239933dd502ab97f973cbfa43d0
abulous <34115349+abulous@users.noreply.github.com>
2018-01-13 20:30:26 -0800

    Add files via upload

---
+++ Loop/Loop.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('Test.mov') 
+frame_counter = 0
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    frame_counter += 1
+    #If the last frame is reached, reset the capture and the frame_counter
+    if frame_counter == cap.get(7):
+        frame_counter = 0 #Or whatever as long as it is the same as next line
+        cap.set(1, 0)
+    
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
