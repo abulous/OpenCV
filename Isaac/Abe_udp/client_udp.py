502b9709e7a16e15c20aa8416935b783a88223e8
inealey <isaacnealey@gmail.com>
2018-03-07 00:41:17 -0800

    version crtl

--- Isaac/Abe_udp/client_udp.py
+++ Isaac/Abe_udp/client_udp.py
@@ -5,20 +5,23 @@ import socket
 import sys
 import pickle
 import struct
+import time
 
 #UDP_IP = "xxx.xxx.x.x" # Inet_4 address for udp
-UDP_IP = "192.168.0.101" # Inet_4 address for udp
+UDP_IP = "192.168.0.102" # Inet_4 address for udp
 UDP_PORT = 8012
 cap=cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
-
+#cap.set(3, 240)
+#cap.set(4, 240)
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # SOCK_DGRAM for udp
-
+time.sleep(2)
 while True:
     ret,frame=cap.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # drop it down to one channel
-    res = cv2.resize(frame,(64, 64), interpolation = cv2.INTER_AREA) # aiming for buffer size of 4096 = 64*64*1
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # drop it down to one channel
+    #res = cv2.resize(frame,(64, 64), interpolation = cv2.INTER_AREA) # aiming for buffer size of 4096 = 64*64*1
+    res = cv2.resize(frame,(140, 140), interpolation = cv2.INTER_AREA) # aiming for buffer size of 4096 = 64*64*1
                                                                      # but check and notice print(data). It is a little bigger.
                                                                      # probably because of resizing interpolation or
                                                                      # from pickling. docs says it adds a little more data
