a925846846fdf82cfc878b146801c9977f9e01c5
Tommy Sharkey <33668799+tlsharkey@users.noreply.github.com>
2018-03-07 23:02:50 -0800

    Program to read and write (simultaneously)
    alternating video files... read from file 1 while writing to file 2, then read from file 2 while writing to file 1

---
+++ Tommy/alternating reading and writing/Tommy_WriteRead.py
@@ -0,0 +1,96 @@
+import cv2
+import numpy as np
+import sys
+import time
+
+cap = cv2.VideoCapture(1)
+reader1 = cv2.VideoCapture('vid1.avi')
+reader2 = cv2.VideoCapture('vid2.avi')
+ret, frame = cap.read()
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+writer1 = cv2.VideoWriter('vid1.avi', fourcc, 20, (640,480), True)
+writer2 = cv2.VideoWriter('vid2.avi', fourcc, 20, (640,480), True)
+
+
+def read1_write2(frame_capture):
+    global reader1, writer2
+    ret, frame_read = reader1.read()
+    
+    writer2.write(frame_capture)
+    if ret:
+        cv2.imshow("vid1.avi Display", frame_read)
+        cv2.moveWindow("vid1.avi Display", 1500,10)
+
+def read2_write1(frame_capture):
+    global reader2, writer1
+    ret, frame_read = reader2.read()
+
+    writer1.write(frame_capture)
+    if ret:
+        cv2.imshow("vid2.avi Display", frame_read)
+        cv2.moveWindow("vid1.avi Display", 1500,70)
+
+def init_read2_write1():
+    global reader1, reader2, writer1, writer2
+    
+    print("\tclosing: r1, w2")
+    reader1.release()
+    writer2.release()
+
+    print("\topening: r2, w1")
+    writer1 = cv2.VideoWriter('vid1.avi', fourcc, 20, (640,480), True)
+    reader2 = cv2.VideoCapture('vid2.avi')
+
+def init_read1_write2():
+    global reader1, reader2, writer1, writer2
+
+    print("\tclosing: r2, w1")
+    reader2.release()
+    writer1.release()
+
+    print("\topening: r1, w2")
+    writer2 = cv2.VideoWriter('vid2.avi', fourcc, 20, (640,480), True)
+    reader1 = cv2.VideoCapture('vid1.avi')
+    
+    
+
+counter = 0
+switch = False
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow("LIVE CAPTURE", frame)
+    cv2.moveWindow("LIVE CAPTURE", 10,10)
+    if cv2.waitKey(33) == 27:
+        break
+
+    
+    if counter%180 == 0:
+        print("switching")
+        if switch:
+            init_read2_write1()
+        else:
+            init_read1_write2()
+        switch = not switch
+
+    
+    if switch:
+        read1_write2(frame)
+    else:
+        read2_write1(frame)
+
+
+    counter += 1
+
+
+    
+cap.release()
+reader1.release()
+reader2.release()
+writer1.release()
+writer2.release()
+
+cv2.destroyAllWindows()
+print("fin")
