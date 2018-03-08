3f4741de083f77df4688af53eb34dc5a592ac65f
Tommy Sharkey <33668799+tlsharkey@users.noreply.github.com>
2018-03-07 23:16:44 -0800

    updating with comments
    and moving some windows around

--- Tommy/alternating reading and writing/Tommy_WriteRead.py
+++ Tommy/alternating reading and writing/Tommy_WriteRead.py
@@ -15,24 +15,31 @@ writer2 = cv2.VideoWriter('vid2.avi', fourcc, 20, (640,480), True)
 
 
 def read1_write2(frame_capture):
+    '''Read from vid1.avi and write passed frame to vid2.avi'''
     global reader1, writer2
     ret, frame_read = reader1.read()
     
     writer2.write(frame_capture)
     if ret:
         cv2.imshow("vid1.avi Display", frame_read)
-        cv2.moveWindow("vid1.avi Display", 1500,10)
+        cv2.moveWindow("vid1.avi Display", 700,10)
 
 def read2_write1(frame_capture):
+    '''Read from vid2.avi and write passed frame to vid1.avi'''
     global reader2, writer1
     ret, frame_read = reader2.read()
 
     writer1.write(frame_capture)
     if ret:
         cv2.imshow("vid2.avi Display", frame_read)
-        cv2.moveWindow("vid1.avi Display", 1500,70)
+        cv2.moveWindow("vid2.avi Display", 700,500)
 
 def init_read2_write1():
+    '''Stop reading from vid1.avi
+    Stop writing to vid2.avi
+    then start reading from vid2.avi
+    and start writing to vid1.avi
+    '''
     global reader1, reader2, writer1, writer2
     
     print("\tclosing: r1, w2")
@@ -44,6 +51,11 @@ def init_read2_write1():
     reader2 = cv2.VideoCapture('vid2.avi')
 
 def init_read1_write2():
+    '''Stop reading from vid2.avi
+    Stop writing to vid1.avi
+    then start reading from vid1.avi
+    and start writing to vid2.avi
+    '''
     global reader1, reader2, writer1, writer2
 
     print("\tclosing: r2, w1")
@@ -60,14 +72,15 @@ counter = 0
 switch = False
 
 while True:
+    #standard live capture
     ret, frame = cap.read()
-    cv2.imshow("LIVE CAPTURE", frame)
-    cv2.moveWindow("LIVE CAPTURE", 10,10)
-    if cv2.waitKey(33) == 27:
+    cv2.imshow("LIVE CAPTURE - for reference", frame)
+    cv2.moveWindow("LIVE CAPTURE - for reference", 10,10)
+    if cv2.waitKey(33) == 27: #esc
         break
-
     
-    if counter%180 == 0:
+    #switch every 180 frames
+    if counter%180 == 0: #this if decides WHEN to switch files
         print("switching")
         if switch:
             init_read2_write1()
@@ -75,12 +88,12 @@ while True:
             init_read1_write2()
         switch = not switch
 
-    
     if switch:
+        #read from vid1.avi and write live capture to vid2.avi
         read1_write2(frame)
     else:
+        #read from vid2.avi and write live capture to vid1.avi
         read2_write1(frame)
-
 
     counter += 1
 
